a0326248db7bad43286efdb8ada8358dfeb75009
Chris Leo <chrisbenevansleo@gmail.com>
2026-04-19 22:43:31 +0300

    fix: Update API types and remove deleted file

--- crop-progress-backend/index.js
+++ crop-progress-backend/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 const authRoutes = require('./routes/auth.js');
 const db = require('./db.js');
 const fieldManagementRoutes = require('./routes/field-management.js');
 
+
 app.use(express.json());
+app.use(cors({
+    origin:['http://localhost:3001', 'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+    allowHeaders: ['Content-Type', 'Authorization',"Cache-Control", "X-Requested-With", "Accept", "Origin","Pragma"],
+    
+}));
+app.options('*', cors()); // Enable pre-flight for all routes
 app.use('/api/auth', authRoutes);
 app.use('/api/field-management', fieldManagementRoutes);
 
